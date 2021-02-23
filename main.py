1f76ac5e20ade1f52d366dadc0aa6537f02dca25
CaptainKey <tp.dev@outlook.fr>
2021-02-23 10:06:57 +0100

    Mise à jour vers MNIST

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ import numpy as np
 class LeNet(nn.Module):
     def __init__(self):
         super(LeNet,self).__init__() 
-        self.conv1 = nn.Conv2d(3,6,5) # [6, 3, 5, 5] [0,0,0,0]
+        self.conv1 = nn.Conv2d(1,6,5) # [6, 3, 5, 5] [0,0,0,0]
         self.conv2 = nn.Conv2d(6,16,5) # [6, 3, 5, 5] [0,0,0,0]
 
         self.relu = nn.ReLU()
         self.pool = nn.MaxPool2d(2,2)
-        self.dense1 = nn.Linear(400,120)
+        self.dense1 = nn.Linear(256,120)
         self.dense2  = nn.Linear(120,84)
         self.dense3 =  nn.Linear(84,10)
     
@@ -23,11 +23,11 @@ class LeNet(nn.Module):
         x = self.pool(x)
 
         x = self.conv2(x)
+
         x = self.relu(x)
         x = self.pool(x)
 
-
-        x = x.view(-1,400)
+        x = x.view(-1,256)
 
         x = self.dense1(x)
         x = self.relu(x)
@@ -44,13 +44,13 @@ if __name__ == '__main__':
     ])
 
     # Chargement des donnees d entrainement
-    set_entrainement = torchvision.datasets.CIFAR10(root='./cifar10',train=True,download=True,transform=pre_processing)
+    set_entrainement = torchvision.datasets.MNIST(root='./cifar10',train=True,download=True,transform=pre_processing)
     # Chargement des donnees de tests
-    set_test = torchvision.datasets.CIFAR10(root='./cifar10',train=False,download=True,transform=pre_processing)
+    set_test = torchvision.datasets.MNIST(root='./cifar10',train=False,download=True,transform=pre_processing)
 
     # DataLoader 
-    loader_entrainement = torch.utils.data.DataLoader(set_entrainement,batch_size=4,num_workers=2)
-    loader_test = torch.utils.data.DataLoader(set_test,batch_size=4,num_workers=2)
+    loader_entrainement = torch.utils.data.DataLoader(set_entrainement,batch_size=1,num_workers=2)
+    loader_test = torch.utils.data.DataLoader(set_test,batch_size=1,num_workers=2)
 
 
     # Sélection de l'unité de calcul
